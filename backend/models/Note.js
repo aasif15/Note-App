ba7076369b3f421408d01137ee85d319601df11d
sp2061 <145824187+sp2061@users.noreply.github.com>
2024-11-18 20:05:58 -0500

    Add files via upload

---
+++ backend/models/Note.js
@@ -0,0 +1,22 @@
+// backend/models/Note.js
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Note', noteSchema);
